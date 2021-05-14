f2dcdfafe550c6442c48957d0eaf45730c1443d0
rdharmarajula <rajesh.dharmarajula@gmail.com>
2021-05-14 12:35:54 +0530

    sleep_in.py

---
+++ venv/parrot_trouble.py
@@ -0,0 +1,10 @@
+'''
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
+We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+Return True if we are in trouble.
+'''
+def parrot_trouble(talking,hour):
+    return  talking and (20 > hour < 7)
+
+value = parrot_trouble(True,6)
+print (value)
